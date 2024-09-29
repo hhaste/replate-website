7e7e3e78001f7e2ba89909f4d918c165856f2fd2
haste <46465883+hhaste@users.noreply.github.com>
2024-09-29 09:15:34 -0400

    Update navbar (added chevron when signed in)

--- comps/Nav.js
+++ comps/Nav.js
@@ -21,13 +21,18 @@ export default function NavBar() {
             {cookie
                 ? <Button
                     href="/dashboard"
-                    className="text-white"
                     as={Link}
                     size="lg"
                     radius="full"
+                    variant="bordered"
                     color="success"
                     showAnchorIcon
+                    startContent={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <polyline points="6 9 12 15 18 9" />
+                    </svg>
+                    }
                 >
+
                     {JSON.parse(cookie).name}
                 </Button>
                 : <Button
